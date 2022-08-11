c8c461e6e17e428d49ab7d3e49c3be40d7bbeedb
Irina <ten7473@mail.ru>
2022-08-11 20:04:18 +0500

    загрузка 2

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,2 +1,62 @@
-package org.example;public class Client {
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private static Socket clientSocket;
+    private static BufferedWriter out;
+    private static BufferedReader in;
+    private static BufferedReader reader;
+
+
+    public void start() throws IOException {
+        clientSocket = new Socket("localhost", 8080);//127.0.0.1
+        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String msg="";
+        boolean cont=true;
+
+        System.out.println("Начинаем викторину. Выбирайте номер правильного ответа на вопрос.");
+        try {
+        while (cont) {
+            msg = in.readLine();
+            if (msg.equals("1")) {
+                for (int i = 0; i < 5; i++) {
+                    msg = in.readLine();
+                    System.out.println(msg);
+                }
+
+                msg = reader.readLine();
+                out.write(msg + "\n");
+                out.flush();
+
+                msg = in.readLine();
+                System.out.println(msg);
+
+                System.out.println("Продолжить викторину? 1 - да, 2 - нет.");
+                msg = reader.readLine();
+                if (msg.equals("2")) {
+                    msg = "stop";
+                    cont = false;
+                } else msg = "cont";
+                out.write(msg + "\n");
+                out.flush();
+            }
+            else {
+                cont = false;
+                System.out.println(msg);
+            }
+          }
+        }
+        catch (IOException e) {System.out.println("Error: "+ e);}
+
+        System.out.println("Викторина окончена.");
+        msg = in.readLine();
+        System.out.println(msg);
+
+        clientSocket.close();
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Hello client");
+        System.out.println("Желаете пройти викторину? 1 - да, 2 - нет.");
+        String msg=reader.readLine();
+
+        if (msg.equals("1")) {
+            Client client = new Client();
+            try {
+                client.start();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
     }
 }
